6e3b4ea66c39e0a47062f28ec997f23fa4edb2ac
pognet <pogo@interbit.pl>
2018-10-12 09:33:05 +0200

    dodanie bool

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include "stdio.h"
 
 int main() {
 
     enum dni {po=10, ni, wt, sr, czw, pi, so};
+
     enum dni dzien_tygodnia;
+    bool czy_byl_obecny = true;
+
     dzien_tygodnia = czw;
     printf("%i\n", dzien_tygodnia);
+
+    printf("%i\n", czy_byl_obecny);
 
 }
