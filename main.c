850a0dee1d6b7fc865ecf8003d8169bac98ae8f6
pognet <pogo@interbit.pl>
2018-10-12 09:18:44 +0200

    start

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    enum dni {po, ni, wt, sr, czw, pi, so};
+    enum dni {po=10, ni, wt, sr, czw, pi, so};
     enum dni dzien_tygodnia;
     dzien_tygodnia = czw;
     printf("%i\n", dzien_tygodnia);
